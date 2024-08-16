6978782e41f2e4cc8fbe820672a418cd9102c21f
Arnadi <49811141+masadiblog@users.noreply.github.com>
2024-08-16 20:17:13 +0700

    Add files via upload

---
+++ files/modules/lazy/image/loading.js
@@ -0,0 +1,48 @@
+function lazyImageLoading(){
+  let lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+  if('IntersectionObserver' in window){
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer){
+      entries.forEach(function(entry){
+        if(entry.isIntersecting){
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove('lazy');
+          lazyImage.classList.add('lazy-loaded');
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+    lazyImages.forEach(function(lazyImage){
+      lazyImageObserver.observe(lazyImage);
+    });
+  }else{
+    let lazyImage = function(){
+      let active = false;
+      if(active === false){
+        active = true;
+        setTimeout(function(){
+          lazyImages.forEach(function(lazyImage){
+            if((lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display !== 'none'){
+              lazyImage.src = lazyImage.dataset.src;
+              lazyImage.classList.remove('lazy');
+              lazyImage.classList.add('lazy-loaded');
+              lazyImages = lazyImages.filter(function(image){
+                return image !== lazyImage;
+              });
+              if(lazyImages.length === 0){
+                document.removeEventListener('scroll', lazyLoad);
+                window.removeEventListener('resize', lazyLoad);
+                window.removeEventListener('orientationchange', lazyLoad);
+              }
+            }
+          });
+          active = false;
+        }, 200);
+      }
+    };
+    document.addEventListener('scroll', lazyLoad);
+    window.addEventListener('resize', lazyLoad);
+    window.addEventListener('orientationchange', lazyLoad);
+  }
+};
+export {lazyImageLoading};
